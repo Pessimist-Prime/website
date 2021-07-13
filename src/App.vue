34c61f7ac92c43548cf16f188d3a4ce3b696d4a4
Claystation <c.wijnekus@gmail.com>
2021-07-13 15:01:31 +0200

    Initial website setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="mb-5">
+      <img class="mx-auto" src="./assets/pp-logo.jpg" />
+      <div class="text-center font-title text-6xl text-primary">
+        Pessimist Prime
+      </div>
+    </div>
+    <div class="flex justify-around">
+      <a href="https://disc.pessimistpri.me"><i class="text-primary3 text-4xl lni lni-discord"></i></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
