d765caa4c2f98ab141baf2e7f4bbb8ee1cd45f11
Claystation <c.wijnekus@gmail.com>
2021-09-01 09:17:00 +0200

    Wowlogs added

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,9 @@
         Pessimist Prime
       </div>
     </div>
-    <div class="flex justify-around">
-      <a href="https://disc.pessimistpri.me"><i class="text-primary3 text-4xl lni lni-discord"></i></a>
+    <div class="flex justify-center">
+      <a href="https://disc.pessimistpri.me" target="_blank" class="mx-3"><i class="text-primary text-4xl lni lni-discord"></i></a>
+      <a href="https://classic.warcraftlogs.com/guild/id/618257" target="_blank" class="mx-3"><img src="./assets/wc-logs.png" class="w-9"></a>
     </div>
   </div>
 </template>
